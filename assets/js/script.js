5843f9bfb1f92360b2b514f94d2002a452e0d5f9
Dattatraya Katkhade <dattatraya.katkhade@proriogosoftware.com>
2025-07-31 16:02:07 +0530

    Updates with experience

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Dattatraya Katkhade";
+            $("#favicon").attr("href", "assets/images/profile.png");
         }
         else {
             document.title = "Come Back To Portfolio";
